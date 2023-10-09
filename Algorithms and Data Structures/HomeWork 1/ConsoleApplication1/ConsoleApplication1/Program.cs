69145623bdc8dc7b34c5f237e39545140076a32d
RezaKusha <reza.kusha1@gmail.com>
2023-10-09 11:19:11 +0330

    Home Work 1 completed

---
+++ Algorithms and Data Structures/HomeWork 1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    internal static class Program
+    {
+        private static string[] _group;
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("How many Sub-String do you want to enter: ");
+                try
+                {
+                    var groupSize = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                    _group = new string[groupSize];
+                    for (var index = 0; index < groupSize; index++)
+                    {
+                        Console.Write("How many Char do you want to enter for this index : ");
+                        try
+                        {
+                            var subStringSize = int.Parse(Console.ReadLine()?? throw new InvalidOperationException());
+                            if (subStringSize < 2)
+                            {
+                                Console.WriteLine("The Limit is at least 2 chars Enter Again");
+                                index--;
+                            }
+                            else
+                            {
+                                _group[index] = (Console.ReadLine()?? throw new InvalidOperationException()).ToLower();
+                                if (_group[index].Length > subStringSize ||
+                                    (!_group[index].Contains('a') && !_group[index].Contains('b') && !_group[index].Contains('c')))
+                                {
+                                    index--;
+                                    Console.WriteLine("Entered string is invalid do it again");
+                                }
+                            }
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Invalid Format try again");
+                        }
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Format try again");
+                }
+
+                if (_group == null) continue;
+                foreach (var subString in _group)
+                {
+                    var minCount = int.MaxValue;
+                    var minSubString = "";
+                    var found = false;
+                    for (var i = 0; i < subString.Length; i++)
+                    {
+                        for (var j = i + 2; j <= subString.Length; j++)
+                        {
+                            var currentSubString = subString.Substring(i, j - i);
+                            var aCount = currentSubString.Count(c => c == 'a');
+                            var bCount = currentSubString.Count(c => c == 'b');
+                            var cCount = currentSubString.Count(c => c == 'c');
+                            if (aCount <= bCount || aCount <= cCount || currentSubString.Length >= minCount) continue;
+                            minCount = currentSubString.Length;
+                            minSubString = currentSubString;
+                            found = true;
+                        }
+                    }
+
+                    if (found)
+                    {
+                        Console.WriteLine(minSubString + " with the size of : " + minCount);
+                    }
+                    else
+                    {
+                        Console.WriteLine(-1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms and Data Structures/HomeWork 1/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int _num = random.Next(1,1000);
+            Kolatz(_num);
+        }
+
+        private static bool PrimNum(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool SumPrime(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            if (PrimNum(sum))
+                return true;
+            else
+                return false;
+        }
+        static void Kolatz(int num)
+        {
+            while (num != 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    if (SumPrime(num))
+                    {
+                        Console.WriteLine(num + " Y ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(num + " N ");
+                    }
+                }
+                else
+                {
+                    num++;
+                    if (SumPrime(num))
+                    {
+                        Console.WriteLine(num + " Y ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(num + " N ");
+                    }                
+                }
+            }
+        }
+    }
+}
