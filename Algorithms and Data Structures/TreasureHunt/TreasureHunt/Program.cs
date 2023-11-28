bba413f09277acd4b8d634983c1fb862ec379304
Codidodido <139787880+Codidodido@users.noreply.github.com>
2023-11-29 00:24:51 +0330

    Edit program class script
    Create a map and player and StartGame method which starts game

--- Algorithms and Data Structures/TreasureHunt/TreasureHunt/Program.cs
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace TreasureHunt
+﻿using System;
+
+namespace TreasureHunt
 {
     internal class Program
     {
         public static void Main(string[] args)
+        {
+            Map map = new Map(5, 5);
+            Player player = new Player(map, 10);
+            StartGame(player,map);
+        }
+
+        public static void StartGame(Player player, Map map)
         {
         }
     }
